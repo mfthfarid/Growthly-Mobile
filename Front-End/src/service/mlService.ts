ff97e15e4366e8e9aeaa5b085360bb05883088b9
mfthfarid <mfthfarid1826@gmail.com>
2025-11-27 19:34:40 +0700

    memperbaiki logo web

--- Front-End/src/service/mlService.ts
+++ Front-End/src/service/mlService.ts
@@ -1,4 +1,4 @@
-const ML_API_BASE_URL = 'http://10.125.173.139:5001'; // Android emulator
+const ML_API_BASE_URL = 'http://192.168.1.7:5001'; // Android emulator
 
 export interface PredictionResponse {
   prediction: number;
